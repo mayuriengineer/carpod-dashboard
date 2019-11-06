f29c4a290d535b0ddb7e0149ad1b38cc168758e7
mayuriengineer <mayuri.singhal@engineermaster.in>
2019-11-06 12:00:04 +0530

    carpod-project

--- src/app/dynamic-form-builder/dynamic-form-builder.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CarbodviewComponent } from './carbodview/carbodview.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    children: [{ path: 'dashboard', component: CarbodviewComponent }]
-  }
-];
